c5034ebf2af4b5730f241cae491e3d437cb5d577
Bukhari Auwal Idris <idrisbukhariauwal@gmail.com>
2020-10-15 04:35:55 +0100

    removed carousel image height

--- myblog/settings.py
+++ myblog/settings.py
@@ -144,7 +144,12 @@ MEDIA_URL = '/media/'
 
 LOGIN_REDIRECT_URL = '/blog/'
 LOGOUT_REDIRECT_URL = '/blog/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_POST = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "idrisbukhariauwal@gmail.com"
+EMAIL_HOST_PASSWORD = "bukhareee"
 
 
 
